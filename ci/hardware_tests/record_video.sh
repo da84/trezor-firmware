2171d87b44c9ff02f321aaae7eadf2abf9a8edf9
Martin Milata <martin@martinmilata.cz>
2020-12-04 19:46:36 +0100

    fixup! ci: ffmpeg tweaks in hardware tests

--- ci/hardware_tests/record_video.sh
+++ ci/hardware_tests/record_video.sh
@@ -6,10 +6,17 @@ fi
 
 OUTPUTFILE=video_$1_$(date +%s).mp4
 INPUTDEVICE=/dev/video0
+HUB_LOC="3-1.4"
+HUB_PORT="3"
 
 if [ "$2" == "start" ]; then
-  echo "[software/video] Starting record to $OUTPUTFILE"
+  echo "[software/video] Toggling camera port $HUB_LOC/$HUB_PORT"
+  uhubctl -l "$HUB_LOC" -p "$HUB_PORT" -a off
+  sleep 5
+  uhubctl -l "$HUB_LOC" -p "$HUB_PORT" -a on
+  sleep 5
   pkill ffmpeg
+  echo "[software/video] Starting record to $OUTPUTFILE"
   ffmpeg -loglevel warning -f oss -f video4linux2 -i $INPUTDEVICE \
     -flush_packets 1 \
     -vf "drawtext=font=Dejavu Sans: \
